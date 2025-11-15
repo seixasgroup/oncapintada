09ded27a9cf4418fb07044eca27739d74e9d03d5
Leandro Seixas <leandro.fisica@gmail.com>
2025-11-15 19:29:48 -0300

    improve pyproject

--- src/oncapintada/__init__.py
+++ src/oncapintada/__init__.py
@@ -24,6 +24,32 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import warnings
+warnings.filterwarnings("ignore")
+
+import os
+import platform
+from datetime import datetime
+from socket import gethostname
+from sys import version as __python_version__
+from sys import executable as __python_executable__
+from ase import __version__ as __ase_version__
+from ase import __file__ as __ase_file__
+from numpy import __version__ as __numpy_version__
+from numpy import __file__ as __numpy_file__
+from pandas import __version__ as __pandas_version__
+from pandas import __file__ as __pandas_file__
+from pytest import __version__ as __pytest_version__
+from pytest import __file__ as __pytest_file__
+from yaml import __version__ as __yaml_version__
+from yaml import __file__ as __yaml_file__
+
+from ase.parallel import parprint as print
+
 from ._version import __version__
+from .bond_counter import BondCounter
+
+
+__all__ = ['BondCounter']
 
 
